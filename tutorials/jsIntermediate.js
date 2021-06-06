35eb7c195e5b450339ff18688b7b0314058d6835
Alan Contreras <alanrcontrerasm@gmail.com>
2021-06-05 21:28:20 -0700

    Tests.js added and extra fetch apis techniques

--- tutorials/jsIntermediate.js
+++ tutorials/jsIntermediate.js
@@ -448,6 +448,18 @@ async function fetchUsers() {
   console.log(data);
 }
 
+
+async function getPersonsInfo(name) {
+  try {
+    const people = await server.getPeople();
+    const person = people.find(person => { return person.name === name });
+    return person;
+  } catch (error) {
+    // Handle the error any way you'd like
+  }
+}
+
+
 fetchUsers();
 
 //APIs
--- tutorials/tests.js
+++ tutorials/tests.js
@@ -1,17 +1,35 @@
+// apiTestfetch();
 
-apiTestfetch();
+apiTestAsync("iron man")
 
-
-function apiTestfetch(){
-
-  const img = document.querySelector('img');
+//api with fetch, promises
+function apiTestfetch() {
+  const img = document.querySelector("img");
 
   //my giphy api key E57A16NSJf4h4kVh33ORbEIlEc5cELFK
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=E57A16NSJf4h4kVh33ORbEIlEc5cELFK&s=iron man', 
-    {mode: 'cors'})
-    .then(response => response.json())
-    .then(response => {
-        img.src = response.data.images.original.url;
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=E57A16NSJf4h4kVh33ORbEIlEc5cELFK&s=iron man",
+    { mode: "cors" }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      img.src = response.data.images.original.url;
     })
-
+    .catch(err=>console.log(err));
 }
+
+async function apiTestAsync(search) {
+  try {
+    const img = document.querySelector("img");
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=E57A16NSJf4h4kVh33ORbEIlEc5cELFK&s=${search}`,
+      { mode: "cors" }
+    );
+    const responseData = await response.json();
+    img.src = responseData.data.images.original.url;
+    console.log("successful " + search)
+  }catch(error){
+      console.log(error);
+  }
+}
+
