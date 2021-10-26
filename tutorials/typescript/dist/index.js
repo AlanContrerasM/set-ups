25612f311fde3072ad2ba51bad450d8e3dac8a61
Alan Contreras <alanrcontrerasm@gmail.com>
2021-10-25 17:01:37 -0700

    Start typescript course

---
+++ tutorials/typescript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//don't forget to install globally npm i -g typescript
+//to compile on terminal tsc index
+//we can also do it watch mode tsc --watch index
+//to create the typscript config on terminal, tsc --init
+//on the config we can for example edit the rootDir and outDir, and now we only call tsc to compile.
+var id = 5;
+// id = "4"; will throw error from both vs code and when compiling
---
+++ tutorials/typescript/src/index.ts
@@ -0,0 +1,10 @@
+//don't forget to install globally npm i -g typescript
+//to compile on terminal tsc index
+//we can also do it watch mode tsc --watch index
+//to create the typscript config on terminal, tsc --init
+//on the config we can for example edit the rootDir and outDir, and now we only call tsc to compile.
+
+
+let id: number = 5;
+// id = "4"; will throw error from both vs code and when compiling
+
